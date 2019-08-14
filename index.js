408a58265072179b97c336c120039377de4e26ef
Yash Singhal <syash5013@gmail.com>
2019-08-15 04:29:49 +0530

    server created

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+const authRoute = require('./auth/authentication');
+const userRoutes = require('./model/UserRoutes');
+
+dotenv.config();
+
+//connect to database
+mongoose.connect(
+    // process.env.DB_CONNECT,
+    "mongodb+srv://ysinghal22:nbsinghalnbyash1@cluster0-b0wd7.mongodb.net/test?retryWrites=true&w=majority",
+    {useNewUrlParser: true},
+    () => console.log("Connected to DB!")
+);
+
+//Middlewares
+
+//body parser
+app.use(express.json());
+
+// authentication route middleware
+app.use('/moodcafe/', authRoute);
+app.use('/moodcafe/user', userRoutes);
+
+app.listen('3000', () => {
+    console.log("Server running on port 3000");
+})
